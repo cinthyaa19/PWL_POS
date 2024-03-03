13183b5721be6061c13c733d4c9499588c75427c
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-03-04 03:13:08 +0700

    Praaktikum-4

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,8 +9,16 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into m_levels(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'Pelanggan', now()]);
+        // DB::insert('insert into m_levels(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'Pelanggan', now()]);
+        // return 'insert data baru berhasil';
 
-        return 'insert data baru berhasil';
+        // $row = DB::update('update m_levels set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris';
+
+        // $row = DB::delete('delete from m_levels where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris';
+
+        $data = DB::select('select * from m_levels');
+        return view('level', ['data' => $data]);
     }
 }
