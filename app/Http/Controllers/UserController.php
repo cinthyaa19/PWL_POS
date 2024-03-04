50f6f1a743a0049776cc51884f6de902fea5e5e3
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-03-04 08:46:07 +0700

    Praktikum-6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'username' => 'customer 1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 1
+        ];
+        UserModel::insert($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
