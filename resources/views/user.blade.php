41af0d15692fe9fed7c449212825e505321a9b30
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-03-11 20:17:14 +0700

    Praktikum-2.7

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -23,7 +23,7 @@
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
             </tr> --}}
-        <h1 style="font-weight: bold;">Data User</h1>
+        {{-- <h1 style="font-weight: bold;">Data User</h1>
         <table border="1">
             {{-- <tr>
                 <td>Jumlah Pengguna</td>
@@ -31,7 +31,7 @@
             <tr>
                 <td>{{ $data }}</td>
             </tr> --}}
-            <tr>
+            {{-- <tr>
                 <td>ID</td>
                 <td>Username</td>
                 <td>Nama</td>
@@ -42,7 +42,37 @@
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
+            </tr> --}}
+        {{-- </table> --}}
+
+        <h1>Data User</h1>
+        <a href="{{ route('/user/tambah') }}">Tambah User</a>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                {{-- <th>Jumlah Pengguna</th> --}}
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+                <th>Kode Level</th>
+                <th>Nama Level</th>
+                <th>Aksi</th>
             </tr>
+            @foreach ($data as $d)
+            <tr>
+                {{-- <td>{{ $count }}</td> --}}
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td></td>
+                <td>{{ $d->level->level_nama }}</td>
+                <td>
+                    <a href="{{ route('/user/ubah', $d->user_id) }}">Ubah</a>
+                    <a href="{{ route('/user/hapus', $d->user_id) }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
         </table>
     </body>
 </html>
