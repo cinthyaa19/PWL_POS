c505a8a5d22d230499bee128729cbc5b48d97f1f
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-03-11 13:42:59 +0700

    Praktikum-2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -25,11 +25,23 @@
             </tr> --}}
         <h1 style="font-weight: bold;">Data User</h1>
         <table border="1">
-            <tr>
+            {{-- <tr>
                 <td>Jumlah Pengguna</td>
             </tr>
             <tr>
                 <td>{{ $data }}</td>
+            </tr> --}}
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+            </tr>
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
         </table>
     </body>
