8e503fb300efea7f4549c76169da491c8d18139f
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-03-15 09:40:38 +0700

    Praktikum 1-Soal 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,4 +132,37 @@
     </body>
 </html> --}}
 
+@extends('layout.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra JS --}}
+
+@push('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@endpush
 
